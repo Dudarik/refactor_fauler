784da69a5a1d273d7da3dc1d5b14b4db6230d16e
Dudarik <Dudarik@yandex.ru>
2023-11-08 21:12:17 +0500

    refactor: logic in amountFor fn

--- src/lib/statement.ts
+++ src/lib/statement.ts
@@ -1,4 +1,4 @@
-import { IInvoice, IPlays } from '../interfaces';
+import { IInvoice, IPerfomance, IPlay, IPlays } from '../interfaces';
 
 export function statement(invoice: IInvoice, plays: IPlays) {
   let totalAmount = 0;
@@ -15,29 +15,7 @@ export function statement(invoice: IInvoice, plays: IPlays) {
   for (const perf of invoice.perfomances) {
     const play = plays[perf.playID];
 
-    let thisAmount = 0;
-
-    switch (play.type) {
-      case 'tragedy':
-        thisAmount = 40000;
-
-        if (perf.audience > 30) thisAmount += 1000 * (perf.audience - 30);
-
-        break;
-
-      case 'comedy':
-        thisAmount = 30000;
-
-        if (perf.audience > 20)
-          thisAmount += 10000 + 500 * (perf.audience - 20);
-
-        thisAmount += 300 * perf.audience;
-
-        break;
-
-      default:
-        throw new Error(`Uncnown type: ${play.type}`);
-    }
+    const thisAmount = amountFor(perf, play);
 
     volumeCredits += Math.max(perf.audience - 30, 0);
 
@@ -50,4 +28,33 @@ export function statement(invoice: IInvoice, plays: IPlays) {
   result += `Amount owed is ${format(totalAmount / 100)}\n`;
   result += `You erned ${volumeCredits}\n`;
   return result;
+
+  function amountFor(aPerfomance: IPerfomance, play: IPlay) {
+    let result = 0;
+
+    switch (play.type) {
+      case 'tragedy':
+        result = 40000;
+
+        if (aPerfomance.audience > 30)
+          result += 1000 * (aPerfomance.audience - 30);
+
+        break;
+
+      case 'comedy':
+        result = 30000;
+
+        if (aPerfomance.audience > 20)
+          result += 10000 + 500 * (aPerfomance.audience - 20);
+
+        result += 300 * aPerfomance.audience;
+
+        break;
+
+      default:
+        throw new Error(`Uncnown type: ${play.type}`);
+    }
+
+    return result;
+  }
 }
